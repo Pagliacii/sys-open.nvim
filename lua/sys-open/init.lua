966d61766326a495d254d4d8248c147892ba5c71
Pagliacii <huangmianrui0310@outlook.com>
2023-07-08 16:48:39 +0800

    feat: open github repo and support more options

--- lua/sys-open/init.lua
+++ lua/sys-open/init.lua
@@ -1,7 +1,13 @@
 local M = {}
 M.config = {
+	open_cmd = nil,
 	debug = false,
 	silent = false,
+	open_dir = true,
+	github_repo = true,
+	github_repo_pattern = "^[%w-]+/[%w-.]+$",
+	link_pattern = "(https?://[%w-_%.%?%.:/%+=&]+)",
+	exclude_chars = "[%[%](){}<>!;,\"']",
 }
 setmetatable(M, {
 	__index = function(_, k)
@@ -9,25 +15,31 @@ setmetatable(M, {
 	end,
 })
 
----Extract the first link from the input string
----@param s string
----@return string
-local function find_link(s)
-	for url in string.gmatch(s, "https?://%S+") do
-		return url
-	end
-end
-
 ---Get the cursor word and extract link or path from it
 ---@return string
-local function get_cursor_word()
-	local cursor_word = string.gsub(vim.fn.expand("<cWORD>"), "[%[%](){}<>!;]", "")
-	local link = find_link(cursor_word)
+function M.get_cursor_word()
+	local cursor_word = vim.fn.expand("<cWORD>")
+	local link = string.match(cursor_word, M.link_pattern)
 	if link then
 		return link
 	end
-	if vim.fn.filereadable(cursor_word) == 1 then
-		return cursor_word
+
+	cursor_word = string.gsub(cursor_word, M.exclude_chars, "")
+	if M.debug then
+		vim.print("[sys-open] cursor word:", cursor_word)
+	end
+
+	target_path = vim.fn.expand(cursor_word)
+	if vim.fn.filereadable(target_path) ~= 0 then
+		return target_path
+	end
+	if M.open_dir and vim.fn.isdirectory(target_path) ~= 0 then
+		return target_path
+	end
+
+	if M.github_repo and string.match(cursor_word, M.github_repo_pattern) ~= nil then
+		--seems like a github repo
+		return string.format("https://github.com/%s", cursor_word)
 	end
 	return ""
 end
@@ -62,8 +74,8 @@ end
 
 ---Open the file/link under cursor with system app
 function M.open()
-	local target = get_cursor_word()
-	if not target then
+	local target = M.get_cursor_word()
+	if target == "" then
 		if M.debug then
 			vim.print("[sys-open] no such target:", vim.fn.expand("<cWORD>"))
 		end
@@ -88,7 +100,12 @@ function M.open()
 end
 
 function M.setup(opts)
-	vim.tbl_extend("force", M.config, opts)
+	M.config = vim.tbl_extend("force", M.config, opts)
+	if M.config.debug then
+		M.config.silent = false
+		M.config.open_dir = true
+		M.config.github_repo = true
+	end
 	vim.api.nvim_create_user_command("SysOpen", M.open, {})
 end
 
